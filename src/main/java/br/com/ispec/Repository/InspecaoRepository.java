e01809c16570989cf7d7feae6caa6ab27ab57ce6
Milton-Pires <mpnn2207@gmail.com>
2026-03-25 18:21:55 -0300

    Conexão com BD + Teste da API via LocalHost

--- src/main/java/br/com/ispec/Repository/InspecaoRepository.java
+++ src/main/java/br/com/ispec/Repository/InspecaoRepository.java
@@ -2,6 +2,8 @@ package br.com.ispec.Repository;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import br.com.ispec.Entities.Inspecao;
 
 public interface InspecaoRepository extends JpaRepository<Inspecao, Long> {
@@ -9,7 +11,8 @@ public interface InspecaoRepository extends JpaRepository<Inspecao, Long> {
 
     List<Inspecao> findByResponsavel_Id(Long responsavelId);
 
-    List<Inspecao> findByEquipamentoCliente_Id(Long clienteId);
+    @Query("SELECT i FROM Inspecao i WHERE i.equipamento.localizacao.cliente.id = :clienteId")
+    List<Inspecao> findByEquipamentoCliente_Id(@Param("clienteId") Long clienteId);
 
     List<Inspecao> findByEquipamentoLocalizacao_Id(Long localizacaoId);
 }
